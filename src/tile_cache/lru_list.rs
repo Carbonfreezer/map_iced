d2b824e1957b5714ae8a0c0048ca29359f1b8a66
Christoph Lürig <christoph.luerig@gmail.com>
2026-08-29 20:25:58 +0200

    first draft

---
+++ src/tile_cache/lru_list.rs
@@ -0,0 +1,149 @@
+//! This module contains as a pseudo double linked list the entries of the data in least recently used priority que.
+
+use std::num::NonZeroU32;
+
+fn get_u32(x: Option<NonZeroU32>) -> Option<u32> {
+    x.map(|x| x.get() - 1)
+}
+
+fn set_u32(x: u32) -> Option<NonZeroU32> {
+    NonZeroU32::new(x + 1)
+}
+
+fn map_u32(x: Option<u32>) -> Option<NonZeroU32> {
+    x.map(|x| NonZeroU32::new(x + 1).unwrap())
+}
+
+#[derive(Debug, Clone, Default)]
+struct LRUEntry {
+    previous: Option<NonZeroU32>,
+    next: Option<NonZeroU32>,
+    hash_content: u64,
+}
+
+#[derive(Debug, Clone, Default)]
+pub struct LastRecentlyUsedList {
+    /// The entry lost with the least recently used stuff.
+    entry_list: Vec<LRUEntry>,
+    /// The stak with the free entries.
+    open_spaces: Vec<u32>,
+    /// The position of the first entry.
+    first_entry: Option<u32>,
+    /// The last entry in the list.
+    last_entry: Option<u32>,
+}
+
+impl LastRecentlyUsedList {
+    /// Takes the entry and moves it to the front.
+    pub fn touch(&mut self, index: u32) {
+        // When we are the first in the list there is nothing to touch.
+        let Some(previous) = get_u32(self.entry_list[index as usize].previous) else {
+            debug_assert_eq!(Some(index), self.first_entry,"Counter check that we are the first entry." );
+            return;
+        };
+
+        // The old first element has now us as the predecessor.
+        if let Some(first) = self.first_entry {
+            self.entry_list[first as usize].previous = set_u32(index);
+        }
+
+        // The old previous now has our next as the follow up.
+        self.entry_list[previous as usize].next = self.entry_list[index as usize].next;
+
+        // Check if we were the last entry on that list.
+        if let Some(next) = get_u32(self.entry_list[index as usize].next) {
+            self.entry_list[next as usize].previous = self.entry_list[index as usize].previous;
+        } else {
+            // In this case we were at the end of the list and the new last entry becomes our previous.
+            debug_assert_eq!(Some(index), self.last_entry,"Counter check that we are the last entry." );
+            self.last_entry = Some(previous);
+        }
+
+        // Now we have to adjust our pointers.
+        self.entry_list[index as usize].previous = None;
+        self.entry_list[index as usize].next = map_u32(self.first_entry);
+        self.first_entry = Some(index);
+    }
+
+    /// Generates a new entry at the top and returns the storage entry.
+    pub fn generate_new_entry(&mut self, data: u64) -> u32 {
+        // First we have to eventually add a new entry.
+        let space = match self.open_spaces.pop() {
+            Some(n) => n,
+            None => {
+                self.entry_list.push(LRUEntry::default());
+                self.entry_list.len() as u32 - 1
+            }
+        };
+        let element = &mut self.entry_list[space as usize];
+        element.hash_content = data;
+        element.previous = None;
+        element.next = map_u32(self.first_entry);
+        self.first_entry = Some(space);
+
+        space
+    }
+
+    /// Frees elements from the Cache and returns the freed elements.
+    pub fn free_elements(
+        &mut self,
+        mut amount_to_free: f32,
+        weight_function: impl Fn(u64) -> f32,
+    ) -> Vec<u64> {
+        let mut result = Vec::new();
+        while let Some(scan) = self.last_entry
+            && amount_to_free > 0.0
+        {
+            let element = &self.entry_list[scan as usize];
+            let content = element.hash_content;
+            result.push(content);
+            amount_to_free -= weight_function(content);
+            self.open_spaces.push(scan);
+            self.last_entry = get_u32(element.previous);
+            if let Some(previous) = self.last_entry {
+                self.entry_list[previous as usize].next = None;
+            }
+        }
+
+        result
+    }
+
+    /// Generates a list from the current cache list from most to least recently used.
+    pub fn generate_usage_list(&self) -> Vec<u64> {
+        let mut result = Vec::new();
+        let mut scan = self.first_entry;
+        while let Some(entry) = scan {
+            result.push(self.entry_list[entry as usize].hash_content);
+            scan = get_u32(self.entry_list[entry as usize].next);
+        }
+
+        result
+    }
+
+    
+    /// Generates an LRU inviction system from an u64 slice handed over.
+    pub fn new(content_slice: &[u64]) -> Self {
+        let mut result = Self::default();
+        if content_slice.is_empty() {
+            return result;
+        }
+
+        let last_element = content_slice.len() - 1;
+
+        let content = content_slice
+            .iter()
+            .enumerate()
+            .map(|(i, x)| LRUEntry {
+                previous: set_u32(i as u32 - 1),
+                next: (i < last_element).then(|| set_u32(i as u32 + 1).unwrap()),
+                hash_content: *x,
+            })
+            .collect();
+        
+        result.entry_list = content;
+        result.first_entry = Some(0);
+        result.last_entry = Some(last_element as u32);
+        
+        result
+    }
+}
---
+++ src/tile_cache/mod.rs
@@ -0,0 +1,3 @@
+//! This system takes care of the asynchronous loading and local caching of map tiles.
+
+mod lru_list;
